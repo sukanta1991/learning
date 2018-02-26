ce88d95231fb424e13642ab9ad558720ae551beb
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-02-26 18:42:32 +0530

    AccountViewModels.cs

---
+++ CSharp/OwinLearn/Application/Models/AccountViewModels.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestLogin.Models
+{
+    public class AccountViewModels
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username
+        {
+            get;
+            set;
+        }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
