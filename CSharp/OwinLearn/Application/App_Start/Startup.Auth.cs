be85b1c87662dfc35d952309e0fc9a0e74f4887e
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-02-26 18:40:51 +0530

    Startup.Auth.cs

---
+++ CSharp/OwinLearn/Application/App_Start/Startup.Auth.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+using System;
+//using TestLogin.Models;
+namespace TestLogin
+{
+    public partial  class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                LogoutPath = new PathString("/Account/LogOff"),
+                ExpireTimeSpan = TimeSpan.FromMinutes(5.0)
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+        }
+    }
+}
