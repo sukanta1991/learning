781a5536bdbae0f151e180121a8a57ae5e5f6c6b
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-01 15:48:21 +0530

    moduleDemo.ts

---
+++ moduleDemo.ts
@@ -0,0 +1,21 @@
+export function MaxDiscountAllowed(noOfProduct: number): number {
+    if (noOfProduct > 5) {
+        return 30;
+    } else {
+        return 10;
+    }
+}
+
+class Utility {
+CalculateAmount(price: number, quantity: number): number {
+return price * quantity;
+}
+
+}
+interface Category {
+getCategory(productId: number): string;
+}
+export const productName:string="Mobile";
+export {Utility,Category};
+
+
