ac2ceee022c0cae0ca8df2886a1a2dc9fdd84c60
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:44:56 +0530

    Create Global.asax.cs

---
+++ CSharp/MVCwithAngular/Application/WebApps/Global.asax.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using System.Security.Claims;
+using System.Web.Helpers;
+
+namespace WebApps
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
+        }
+    }
+}
