1a06aeecad591a4f48076b4964c5195624b07a19
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:30:52 +0530

    Create LoginByUsernamePassword_Result.cs

---
+++ CSharp/MVCwithAngular/Application/RepoModel/LoginByUsernamePassword_Result.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace RepoModel
+{
+       public class LoginByUsernamePassword_Result
+        {
+                public int id { get; set; }
+                public string username { get; set; }
+                public string password { get; set; }
+        }
+}
