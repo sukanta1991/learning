b9c39fdbcaed049c440e24fca9bcf4c9e83426cd
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:31:46 +0530

    Create ufn_getSkills_Result.cs

---
+++ CSharp/MVCwithAngular/Application/RepoModel/ufn_getSkills_Result.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace RepoModel
+{
+    public class ufn_getSkills_Result
+    {
+        public string userName { get; set; }
+        public string Skills { get; set; }
+        public string trained { get; set; }
+    }
+}
