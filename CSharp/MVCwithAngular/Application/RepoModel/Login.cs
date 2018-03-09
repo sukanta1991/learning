377b5b7ff6de9edad1b57d68ac3aa2fe1da936f4
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:30:31 +0530

    Create Login.cs

---
+++ CSharp/MVCwithAngular/Application/RepoModel/Login.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace RepoModel
+{
+        public class Login
+        {
+                public int id { get; set; }
+                public string username { get; set; }
+                public string password { get; set; }
+        }
+}
