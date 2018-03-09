fa5065dec193fe3878b39bb55efc92e2ab60546f
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:42:38 +0530

    Create Startup.cs

---
+++ CSharp/MVCwithAngular/Application/WebApps/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartupAttribute(typeof(WebApps.Startup))]
+namespace WebApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
