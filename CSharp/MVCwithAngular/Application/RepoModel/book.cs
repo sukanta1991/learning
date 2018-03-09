ed9ccc232dfe9eecf4446cd4929349a1b56396b2
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-03-09 10:29:32 +0530

    Create book.cs

---
+++ CSharp/MVCwithAngular/Application/RepoModel/book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RepoModel
+{
+    public class book
+    {
+                public int bookId { get; set; }
+                public string bookName { get; set; }
+                public string authors { get; set; }
+                public string readbook { get; set; }
+     }
+}
