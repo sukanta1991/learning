30564456d7f9b99caac8e4db722a6e2e45c1f7f1
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-06-14 17:01:29 +0530

    Create TaskModel.cs

---
+++ Mobile/Xamarin/SampleApp/Models/TaskModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testApp.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public int Duration { get; set; }
+    }
+}
