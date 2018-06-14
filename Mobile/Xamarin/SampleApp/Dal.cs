039075ea7143b5b6a574d6e6f8ee91a381501e70
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-06-14 17:05:56 +0530

    Create Dal.cs

---
+++ Mobile/Xamarin/SampleApp/Dal.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace testApp
+{
+    class Dal
+    {
+        //System.Data.SqlClient.SqlConnection con;
+        //SqlConnection con;
+        //public Dal()
+        //{
+        //   // con = new SqlConnection("Data Source=db;Initial Catalog=user;Persist Security Info=True;User ID=user;Password=pass");
+        //}
+        //public List<string> testDB()
+        //{
+        //    con = new SqlConnection("Data Source=db;Initial Catalog=user;Persist Security Info=True;User ID=user;Password=pass");
+        //    SqlCommand cmd = new SqlCommand("select distinct Category from tab " , con);
+        //    try
+        //    {
+        //        con.Open();
+                
+
+        //    }
+        //    catch
+        //    {
+        //        con.Close();
+        //    }
+        //}
+    }
+}
