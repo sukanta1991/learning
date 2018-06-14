a25875709f9fdd00a206e38153b7336b0c570747
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-06-14 17:07:39 +0530

    Create Person.cs

---
+++ Mobile/Xamarin/SampleApp/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testApp
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
