689f25bb9e7c9145e0899cb63f31994205b5359b
Sukanta  Saha <33719073+sukanta1991@users.noreply.github.com>
2018-06-14 17:01:04 +0530

    Create TextToColorConverter.cs

---
+++ Mobile/Xamarin/SampleApp/Converters/TextToColorConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace testApp.Converters
+{
+    class TextToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var text = value.ToString();
+                if (text.Contains("UI"))
+                    return Color.Navy;
+            }
+            return Color.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
